87d8b98d866a4b70d46e01cd0253737bf5b59327
Colton Brenneman <colton.brenneman@gmail.com>
2023-08-24 18:43:28 -0600

    finished model file

---
+++ Assessment4/Assessment4/Controllers/ModelControllers/NetworkController.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkController.swift
+//  Assessment4
+//
+//  Created by Colton Brenneman on 6/20/23.
+//
+
+import Foundation
+
+struct NetworkController {
+
+} //End of struct
---
+++ Assessment4/Assessment4/Models/Rover.swift
@@ -0,0 +1,49 @@
+//
+//  Rover.swift
+//  Assessment4
+//
+//  Created by Colton Brenneman on 6/20/23.
+//
+
+import Foundation
+
+struct TopLevelDictionary: Decodable {
+    let photos: [Photo]
+}
+
+struct Photo: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case sol
+        case imagePath = "img_src"
+        case earthDate = "earth_date"
+        case rover
+    }
+    let id: Int
+    let sol: Int
+    let rover: Rover
+    let imagePath: String
+    let earthDate: String
+}
+
+struct Rover: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case roverID = "id"
+        case name
+        case launchDate = "launch_date"
+        case camera
+    }
+    let roverID: Int
+    let name: String
+    let launchDate: String
+    let camera: [Camera]
+}
+
+struct Camera: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case cameraName = "name"
+        case fullCameraName = "full_name"
+    }
+    let cameraName: String
+    let fullCameraName: String
+}
